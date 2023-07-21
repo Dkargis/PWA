7b8ddb575a2c6ebdd2a813069cf50fb1d288cc8e
Dane Kargis <dane.kargis@gmail.com>
2023-07-20 21:54:59 -0600

    fixed a bug

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,10 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './src-sw.js',
+      }),
       new MiniCssExtractPlugin(),
       new WebpackPwaManifest({
         name: 'TODOs',
